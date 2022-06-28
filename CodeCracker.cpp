02a6ac54f912a83811d0c0c8685ccdfb2783be3c
gabriochee <gabypla2005@gmail.com>
2022-06-28 13:11:19 +0200

    Finished the program

--- CodeCracker.cpp
+++ CodeCracker.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <string>
 
+bool same(char* target, char* prototype, int target_length) {
+    for (int i = 0; i < target_length; i++) {
+        if (prototype[i] != target[i]) { return false; }
+    }
+    return true;
+}
+
 int main()
 {
     char caracters[63] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    //for (int i = int('A'); i <= int('Z'); i++) {
-    //    caracters[i - int('A')] = char(i);
-    //}
-    //for (int i = int('a'); i <= int('z'); i++) {
-    //    caracters[26 + i - int('a')] = char(i);
-    //}
-    //for (int i = int('0'); i <= int('9'); i++) {
-    //    caracters[52 + i - int('0')] = char(i);
-    //}
-    char mdp[] = {'a','l','a'};
+    char mdp[] = "1234";
     char test[26] = { 'a' };
     int pointer = 0;
     int index = 0;
     bool cycle_ended = false;
-    while (test[0] != test[1] != test[2] != 'z') {
+    while (!same(mdp,test,4)) {
         if (!cycle_ended) {
             test[pointer] = caracters[index];
             if (index >= 61) {
@@ -31,25 +29,27 @@ int main()
         }
         else {
             int length = 0;
-            bool new_letter = false;
+            bool added = false;
             while (test[pointer] == '9') {
                 test[pointer] = 'A';
                 pointer--;
+                length++;
                 if (pointer < 0) {
-                    pointer = length+1;
+                    pointer = length;
+                    std::cout << length << '\n';
                     test[pointer] = 'A';
-                    new_letter = true;
+                    added = true;
                     break;
                 }
-                length++;
             }
-            if (!new_letter) {
+            if(!added) {
                 int pos = 0;
-                for (pos; pos < 62; pos++) {
-                    if (caracters[pos] == test[pointer]) { break; }
+                for (pos; pos <= 61; pos++) {
+                    if (caracters[pos] == test[pointer]) { pos++; break; }
                 }
-                test[pointer] = (pos < 61) ? caracters[pos + 1] : 'A';
+                test[pointer] = caracters[pos];
                 pointer++;
+                while (test[pointer] == 'A' && test[pointer + 1] != 0) { pointer++; }
             }
             cycle_ended = false;
         }
